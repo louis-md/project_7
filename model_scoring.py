9944f0034d75fdba9ae32a6670b9b67285542440
Lucien Stephane Ld <stephaneledan@gmail.com>
2023-05-26 11:51:34 +0200

    Add scoring module

---
+++ model_scoring.py
@@ -0,0 +1,22 @@
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score,
+    precision_recall_curve, roc_curve, roc_auc_score
+)
+
+import pprint
+
+from matplotlib import pyplot as plt
+
+
+def scoring(train, pred):
+        pprint.pprint(f'The accuracy score of the model is: {accuracy_score(train, pred)} ')
+        pprint.pprint(f'The precision score of the model is: {precision_score(train, pred)} ')
+        pprint.pprint(f'The recall score of the model is: {recall_score(train, pred)} ')
+        pprint.pprint(f'The ROC AUC score of the model is: {roc_auc_score(train, pred)} ')
+
+        fp, tp, _ = roc_curve(train, pred)
+        fig = plt.plot(fp, tp)
+        plt.xlabel('False positives')
+        plt.ylabel('True positives')
+        fig.show();
+
